dba978bbf4eb098ccf42add52e6f71feb4f7e127
Aishwarya Tupe <69886925+ash-1703@users.noreply.github.com>
2024-09-26 16:21:56 -0500

    updated logo

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          EchoSpace
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
